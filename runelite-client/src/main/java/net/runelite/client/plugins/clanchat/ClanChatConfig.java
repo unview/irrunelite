aa94ca503d14a895e41848ed8e034846064bd358
256 <De.pker4@gmail.com>
2018-05-30 17:15:55 -0700

    Leave and Enter CC Message
    Displays a message in the CC if a player has entered or left the channel.

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/clanchat/ClanChatConfig.java
@@ -0,0 +1,38 @@
+
+package net.runelite.client.plugins.clanchat;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+import java.awt.*;
+
+@ConfigGroup(
+		name = "Clan Chat",
+		keyName = "clanchat",
+		description = "Configuration for the clan chat plugin"
+)
+public interface ClanChatConfig extends Config
+{
+	@ConfigItem(
+			position = 0,
+			keyName = "showEnterAndLeaveMessages",
+			name = "Show member enter and leave messages",
+			description = "Configures whether or not to display messages informing who has entered and left the clan chat"
+	)
+	default boolean showEnterAndLeaveMessages()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			position = 1,
+			keyName = "LAEC",
+			name = "Message Color",
+			description = "Configures the color of the Leave and Enter message"
+	)
+	default Color getLAEC()
+	{
+		return new Color(0, 100, 255);
+	}
+}
